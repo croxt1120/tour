07bd42d74a3d33fe2116fa1cd516a5ec374d52ef
royleej9 <royleej9@gmail.com>
2016-02-25 00:15:05 +0000

    add sample

--- server/api/test/index.js
+++ server/api/test/index.js
@@ -3,24 +3,73 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 
+
 var PATH_DATAS_DIR = '../../datas/';
 
-router.get('/test.do', function(req, res, next) {
-
-    var filePath = path.resolve(__dirname, PATH_DATAS_DIR + 'test.json');
-    var data = {};
-    console.log(filePath);
-    
-    if(!fs.existsSync(filePath)) {
-        console.log("File not found");
-        data['isSuccess'] = false;
-    } else {
-        var text = fs.readFileSync(filePath,'utf8');
-        data['data'] = JSON.parse(text);
-        data['isSuccess'] = true;
-    }
-    
-    return res.json(data);
+/*
+router.param( function(req, res, next) {
+   console.log('data');
+   
+   console.log(req.params); 
+   next();
 });
+*/
+
+
+
+
+
+// router.get('/test.do', function(req, res, next) {
+//     var filePath = path.resolve(__dirname, PATH_DATAS_DIR + 'test.json');
+//     var data = {};
+//     console.log(filePath);
+//     if(!fs.existsSync(filePath)) {
+//         console.log("File not found");
+//         data['isSuccess'] = false;
+//     } else {
+//         var text = fs.readFileSync(filePath,'utf8');
+//         data['data'] = JSON.parse(text);
+//         data['isSuccess'] = true;
+//     }
+//     return res.json(data);
+// });
+
+// router.param('fileName', function(req, res, next, id) {
+//     console.log('param');
+//     console.log(req.params);
+//     console.log(req.body);
+//     console.log(id);
+//     console.log("-----------------");
+//   next();
+// });
+
+
+router.get('/post/:fileName', function(req, res, next){
+    console.log('dfdfdf');
+    return res.json({test: 'get'});
+});
+
+
+// router
+// .route('/post/:fileName')
+// .get(function(req, res, next){
+//     console.log('dfdfdf');
+//     return res.json({test: 'get'});
+// })
+/*.post(function(req, res, next) {
+
+    console.log('============================');
+    console.log('post');
+    console.log(req.params);
+    console.log(req.body);
+    console.log('-----------------');
+    return res.json({test:'post'});    
+});*/
+
+/*
+router.post('/post/:fileName', function(req, res, next) {
+
+});
+*/
 
 module.exports = router;

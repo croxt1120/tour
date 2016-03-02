cd10286eeb9a9785640098dfab1e8be4bec5a062
kdb316 <kdb316@naver.com>
2016-03-02 03:13:58 +0000

    dbkim

--- server/routes/code/index.js
+++ server/routes/code/index.js
@@ -42,63 +42,30 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:fileName', function(req, res, next) {
-        // console.log(req.params);
-        // console.log(req.params.fileName);
-        
-        var filePath = path.resolve(__dirname, PATH_DATAS_DIR + req.params.fileName + '.json');
-     
-        console.log(req.body);
-        console.log(req.params);
-        console.log( req.params.fileName);
-        
-        // var filePath = path.resolve(__dirname, PATH_DATAS_DIR + req.params.fileName + '.json');
-        // // var filePath = path.resolve(__dirname, PATH_DATAS_DIR + 'food.json');
-        
-        var data = {};
-        // console.log(filePath);
-        
-        if(!fs.existsSync(filePath)) {
-            console.log("File not found");
-            data['isSuccess'] = false;
-        } else {
-            var text = fs.readFileSync(filePath,'utf8');
-            // console.log("## text -> " + text);
-            data['data'] = JSON.parse(text);
-            data['isSuccess'] = true;
-        }
-        
-        return res.json(data);
-});
-    
-router.post('/:fileName', function(req, res, next) {
-    console.log(req.body);
-    console.log(req.params);
-    // console.log(req.params.fileName);
-    
     var filePath = path.resolve(__dirname, PATH_DATAS_DIR + req.params.fileName + '.json');
-    // // var filePath = path.resolve(__dirname, PATH_DATAS_DIR + 'food.json');
-    
-    // var data = {};
-    // console.log(filePath);
+ 
+    var data = {};
     
     if(!fs.existsSync(filePath)) {
         console.log("File not found");
-        // data['isSuccess'] = false;
+        data['isSuccess'] = false;
     } else {
-    //     var text = fs.readFileSync(filePath,'utf8');
-    //     console.log("## text -> " + text);
-    //     data['data'] = JSON.parse(text);
-    //     data['isSuccess'] = true;
-        // var myData = [{name: "국밥", price:"5000", explain:"good"},{name: "초밥", price:"7000", explain:"bad"}];
-        fs.writeFile(filePath, req.body.saveData, 'utf-8', function(err) {
-           if(err)
-            console.log(err);
-            
-        });
+        var text = fs.readFileSync(filePath,'utf8');
+        data['data'] = JSON.parse(text);
+        data['isSuccess'] = true;
     }
     
-    return res.json({data: 3});
-    // return res.json(data);
+    return res.json(data);
+});
+    
+router.post('/:fileName', function(req, res, next) {
+    var filePath = path.resolve(__dirname, PATH_DATAS_DIR + req.params.fileName + '.json');
+    
+    var data = {};
+   
+    fs.writeFile(filePath, req.body.saveData, 'utf-8');
+    data['isSuccess'] = true;
+    return res.json(data);
 });
     
 

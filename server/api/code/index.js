0ebf24cf8f2f9aa70ca5512ebd653d6fd420b8af
kdb316 <kdb316@naver.com>
2016-03-02 01:38:17 +0000

    dbkim

--- server/api/code/index.js
+++ server/api/code/index.js
@@ -5,64 +5,37 @@ var path = require('path');
 
 var PATH_DATAS_DIR = '../../datas/';
 
+/**
+ *  read data
+ */
 router.get('/:fileName', function(req, res, next) {
-        // console.log(req.params);
-        // console.log(req.params.fileName);
-        
         var filePath = path.resolve(__dirname, PATH_DATAS_DIR + req.params.fileName + '.json');
      
-        console.log(req.body);
-        console.log(req.params);
-        console.log( req.params.fileName);
-        
-        // var filePath = path.resolve(__dirname, PATH_DATAS_DIR + req.params.fileName + '.json');
-        // // var filePath = path.resolve(__dirname, PATH_DATAS_DIR + 'food.json');
-        
         var data = {};
-        // console.log(filePath);
         
         if(!fs.existsSync(filePath)) {
             console.log("File not found");
             data['isSuccess'] = false;
         } else {
             var text = fs.readFileSync(filePath,'utf8');
-            // console.log("## text -> " + text);
             data['data'] = JSON.parse(text);
             data['isSuccess'] = true;
         }
         
         return res.json(data);
 });
-    
+/**
+ *  save data
+ */
 router.post('/:fileName', function(req, res, next) {
-    console.log(req.body);
-    console.log(req.params);
-    // console.log(req.params.fileName);
-    
     var filePath = path.resolve(__dirname, PATH_DATAS_DIR + req.params.fileName + '.json');
-    // // var filePath = path.resolve(__dirname, PATH_DATAS_DIR + 'food.json');
     
-    // var data = {};
-    // console.log(filePath);
+    var data = {};
+   
+    fs.writeFile(filePath, req.body.saveData, 'utf-8');
+    data['isSuccess'] = true;
     
-    if(!fs.existsSync(filePath)) {
-        console.log("File not found");
-        // data['isSuccess'] = false;
-    } else {
-    //     var text = fs.readFileSync(filePath,'utf8');
-    //     console.log("## text -> " + text);
-    //     data['data'] = JSON.parse(text);
-    //     data['isSuccess'] = true;
-        // var myData = [{name: "국밥", price:"5000", explain:"good"},{name: "초밥", price:"7000", explain:"bad"}];
-        fs.writeFile(filePath, req.body.saveData, 'utf-8', function(err) {
-           if(err)
-            console.log(err);
-            
-        });
-    }
-    
-    return res.json({data: 3});
-    // return res.json(data);
+    return res.json(data);
 });
     
 

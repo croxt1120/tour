05466f053997b5d62e960cee825d46d36d966516
carran <carran@yescnc.co.kr>
2017-06-02 08:43:44 +0000

    도장 이미지 추가

--- client/js/app/views/print/PrintView.js
+++ client/js/app/views/print/PrintView.js
@@ -281,17 +281,6 @@ define([
 			var result = $("<div></div>");
 			var html = this.$('.content').html();
 			result.append(html);
-			// if(data.mail.includes("lycos.co.kr") || data.mail.includes("live.co.kr") || data.mail.includes("nate.com")){
-			
-			// }else{
-				// result.find(".coverimg").remove();
-				// result.find(".cover").css({
-				// 	"background-image": "url("+window.location.protocol + "//" + window.location.host+"/img/cover.jpg)",
-				// 	"background-size": "cover",
-				// 	"width": "calc(100% - 260px)",
-				// 	"height": "197px"
-				// });
-			// }
 			data.html = result.html();
 			
 			$.post("/mail", data, function(res) {
